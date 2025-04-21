4b05143a3b0348e6297ebdced48f4b9ce328b2ba
iwtba4188 <X69599596@gmail.com>
2025-04-21 20:43:40 +0800

    feat: prototype finished!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import os
+import google.generativeai as genai
 
 # from openai import OpenAI
 import time
-import re
 
 placeholderstr = "Please input your command"
 user_name = "Shihtl"
@@ -15,15 +16,51 @@ def stream_data(stream_str):
         time.sleep(0.15)
 
 
+def read_system_prompt(file_path: str) -> str:
+    with open(file_path, "r", encoding="utf-8") as file:
+        system_prompt = file.read()
+    return system_prompt
+
+
+def generate_response_gemini(prompt: str) -> str:
+
+    system_prompt = read_system_prompt("./system_prompt.txt")
+
+    genai.configure(api_key=st.secrets["gemini_api_key"])
+
+    # Create the model
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 65536,
+        "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+        model_name="gemini-2.5-flash-preview-04-17",
+        generation_config=generation_config,
+        system_instruction=system_prompt,
+    )
+
+    chat_session = model.start_chat()
+
+    response = chat_session.send_message(prompt)
+
+    print(response.text)
+
+    return response.text
+
+
 def main():
     st.set_page_config(
-        page_title="K-Assistant - The Residemy Agent",
+        page_title="Assistant - The Residemy Agent",
         layout="wide",
         initial_sidebar_state="auto",
         menu_items={
             "Get Help": "https://streamlit.io/",
-            "Report a bug": "https://github.com",
-            "About": "About your application: **Hello world**",
+            "Report a bug": "https://github.com/iwtba4188/11320ISS507300_Assistant",
+            "About": "This is a demo chatbot about your pets.",
         },
         page_icon="img/favicon.ico",
     )
@@ -68,19 +105,12 @@ def main():
                 except:
                     st_c_chat.chat_message(msg["role"]).markdown((msg["content"]))
 
-    def generate_response(prompt):
-        pattern = r"\b(i(\'?m| am| feel| think i(\'?)?m)?\s*(so\s+)?(stupid|ugly|dumb|idiot|worthless|loser|useless))\b"
-        if re.search(pattern, prompt, re.IGNORECASE):
-            return "Yes, you are!"
-        else:
-            return f"You say: {prompt}."
-
     # Chat function section (timing included inside function)
     def chat(prompt: str):
         st_c_chat.chat_message("user", avatar=user_image).write(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = generate_response(prompt)
+        response = generate_response_gemini(prompt)
         # response = f"You type: {prompt}"
         st.session_state.messages.append({"role": "assistant", "content": response})
         st_c_chat.chat_message("assistant").write_stream(stream_data(response))
